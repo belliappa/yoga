e68d32f8619793f795497fa952ea29c8db3c426f
ratan <belliappa@gmail.com>
2013-08-29 01:02:16 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yoga::Application.config.secret_key_base = '5fca490f639b6561881444a5b0fffaaa3156eb6c74293fda4d34408705a211787ecdbf8610bd98c2f91ad1fdff94fc2f9832fe5cfe52b28e64d600b663f5f34b'
